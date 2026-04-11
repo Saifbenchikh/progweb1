95b705200e5982af4953aa7eb5d1e391f5489d9b
BRYOCHE <benchikhislam1@gmail.com>
2026-04-11 20:52:27 +0200

    calcul et affichage du score du quiz en JavaScript

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     
     const quizForm = document.getElementById('quiz-form');
+    const scoreDisplay = document.getElementById('score-display');
     
     if (quizForm) {
         quizForm.addEventListener('submit', (event) => {
             event.preventDefault();
-            alert("Le formulaire est intercepté ! On calculera le score à la prochaine étape.");
+            
+            let score = 0;
+            
+            const q1 = document.querySelector('input[name="q1"]:checked').value;
+            const q2 = document.querySelector('input[name="q2"]:checked').value;
+            const q3 = document.querySelector('input[name="q3"]:checked').value;
+            
+            if (q1 === 'non') {
+                score = score + 1;
+            }
+            
+            if (q2 === 'oui') {
+                score = score + 1;
+            }
+            
+            if (q3 === 'non') {
+                score = score + 1;
+            }
+            
+            scoreDisplay.textContent = "Votre score est de " + score + " / 3";
         });
     }
 
