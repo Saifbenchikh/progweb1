79c75053826191ea2ffb011b111e0bc4421858bf
Saif Benchikh <benchikhislam1@gmail.com>
2026-06-05 14:19:01 +0200

    Création de la page admin.php avec son titre

---
+++ concours/admin.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Administration</title>
+</head>
+<body>
+    <h1>Administration du Concours</h1>
+    <p>Page de gestion réservée à l'administrateur.</p>
+</body>
+</html>
