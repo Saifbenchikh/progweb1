9143b7f869ea24e551c672017919a5eac6822efd
Saif Benchikh <benchikhislam1@gmail.com>
2026-06-05 15:41:45 +0200

    Création de hasard.php et ajout des liens de navigation

---
+++ concours/hasard.php
@@ -0,0 +1,14 @@
+<?php require_once __DIR__ . '/outils.php'; ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Question au hasard</title>
+</head>
+<body>
+    <a href="index.php">Accueil</a>
+    
+    <h2>Prêt pour une question ?</h2>
+    <p>La question s'affichera ici à la prochaine étape...</p>
+</body>
+</html>
--- concours/index.php
+++ concours/index.php
@@ -1,9 +1,4 @@
-<?php
-require_once __DIR__ . '/outils.php';
-?>
-
-
-
+<?php require_once __DIR__ . '/outils.php'; ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -11,6 +6,8 @@ require_once __DIR__ . '/outils.php';
     <title>Question pour un champion</title>
 </head>
 <body>
-    <h1>Bienvenue sur le Concours à 200iq !</h1>
+    <h1>Bienvenue sur le Concours 200iq !</h1>
+    
+    <a href="hasard.php">Une question au hasard</a>
 </body>
 </html>
