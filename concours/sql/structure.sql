9611d6fbc19d51586ffee6a099c04c49a912bc95
Saif Benchikh <benchikhislam1@gmail.com>
2026-06-05 10:48:24 +0200

    Création de la table question

--- concours/sql/structure.sql
+++ concours/sql/structure.sql
@@ -1,1 +1,11 @@
 CREATE DATABASE concours;
+
+USE concours;
+
+CREATE TABLE question (
+    id INT AUTO_INCREMENT,
+    intitule VARCHAR(255),
+    reponse VARCHAR(100),
+    points INT CHECK (points >= 10 AND points <= 100),
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
